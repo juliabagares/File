b34ed9621d64e5d6adf8f5b1da74bc2813c79e4c
Julia Bagares <juliabagares2007@gmail.com>
2026-04-28 02:36:36 +0800

    source file

--- odd_and_even.py
+++ odd_and_even.py
@@ -3,3 +3,11 @@ class Color:
     CYAN = '\033[96m'
     GREEN = '\033[92m'
     RESET = '\033[0m'
+
+class NumberProcessor:
+    def __init__(self, source_file="numbers.txt"):
+        self.source = source_file
+        self.even_numbers = []
+        self.odd_numbers = []
+        
+
