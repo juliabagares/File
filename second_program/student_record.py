b799cb00d53e952cdfc1cc1dbc8a7c0774f99eed
Julia Bagares <juliabagares2007@gmail.com>
2026-04-29 01:35:06 +0800

    students data

--- second_program/student_record.py
+++ second_program/student_record.py
@@ -5,4 +5,7 @@ class Student:
     name: str
     gwa: float
 
-    
+class StudentRecord:
+    def __init__(self, source_file = students.txt):
+        self.source = source_file
+        self.roster = []
