77b0279a9a18c885079ad9162b8c009caa542951
Julia Bagares <juliabagares2007@gmail.com>
2026-04-29 11:37:01 +0800

    indentify if even or odd then square or cube

--- fourth_program/integer_processor.py
+++ fourth_program/integer_processor.py
@@ -3,3 +3,9 @@ class IntegerProcessor:
         self.source = source
         self.evens_file = "double.txt"
         self.odds_file = "triple.txt"
+
+    @staticmethod
+    def _process_logic(value):
+        if value % 2 == 0:
+            return "even", value ** 2
+        return "odd", value ** 3
