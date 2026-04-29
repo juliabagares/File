d2743b16cb8c3e1032ec290b15412d85d53cfc2e
Julia Bagares <juliabagares2007@gmail.com>
2026-04-29 10:40:49 +0800

    welcome message

--- third_program/multiple_line_text.py
+++ third_program/multiple_line_text.py
@@ -2,3 +2,6 @@ class LifeRecorder:
     def __init__(self, filename="mylife.txt"):
         self.filename = filename
 
+    def start_session(self):
+        print(f"Welcome to LifeRecorder(saving to {self.filename})")
+    
