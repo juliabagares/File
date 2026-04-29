f78c5c24c363705ec8e4670375927a5c23402f03
Julia Bagares <juliabagares2007@gmail.com>
2026-04-29 11:43:02 +0800

    file distribution

--- fourth_program/integer_processor.py
+++ fourth_program/integer_processor.py
@@ -9,3 +9,11 @@ class IntegerProcessor:
         if value % 2 == 0:
             return "even", value ** 2
         return "odd", value ** 3
+
+    def run(self):
+        try:
+        with open (self.source, "r") as src, \
+            open(self.evens_file, "w") as evens, \
+            open(self.odds_file, "w") as odds:
+            for line in src:
+                if not line.strip(): continue
